15e86447bb7fff9e88cbffc6964ff53f5b4b1c4e
TheNyxieCreator <adryantheprogrammer@gmail.com>
2025-04-19 22:18:41 -0300

    nyxieworldos-Dev_0.0.2

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -4,12 +4,12 @@ void kernel_main() {
   print_clear();
                   //Text Color        //Background Color
   print_set_color(PRINT_COLOR_WHITE, PRINT_COLOR_BLACK);
-  print_str(" +-------------------------------------------------------------+\n");
-  print_str(" |                                                             |\n");
-	print_str(" |             [NyxieworldOS Version 0.0.2 <Allana>]           |\n");
-	print_str(" |                2024-2025 Nyxieworld Project                 |\n");
-	print_str(" |             (C) Copyright Nyxieworld Productions            |\n");
-	print_str(" |                                                             |\n");
-	print_str(" +-------------------------------------------------------------+\n");
-  print_str(" [                  Development Release 0.0.2                  ]\n");
+  print_str(" +---------------------------------------------------------------------------+\n");
+  print_str(" |                                                                           |\n");
+	print_str(" |                  [NyxieworldOS Version 0.0.2 <Allana>]                    |\n");
+	print_str(" |                      2024-2025 Nyxieworld Project                         |\n");
+	print_str(" |                   (C) Copyright Nyxieworld Productions                    |\n");
+	print_str(" |                                                                           |\n");
+	print_str(" +---------------------------------------------------------------------------+\n");
+  print_str(" [                         Development Release 0.0.2                         ]\n");
 }
