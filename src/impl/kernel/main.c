11906d0d75cfa58a36e170c79a1c4ef2f30f0315
TheNyxieCreator <adryantheprogrammer@gmail.com>
2025-04-19 22:13:43 -0300

    nyxieworldos-Dev_0.0.2
    Nyxieworld OS 0.0.2 Development cycle started

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -2,16 +2,14 @@
 
 void kernel_main() {
   print_clear();
-  print_set_color(PRINT_COLOR_BLACK, PRINT_COLOR_WHITE);
-  print_str("# Bem-vindo, ao Nyxieworld OS com suporte 64bit! \n");
-  print_str("# (C) Copyright 2024-2025 Nyxieworld Project \n");
-  print_str(" \n");
-  print_str(" +--------------------------------------------+\n");
-	print_str(" |   [NyxieworldOS Version 0.0.1 <Melissa>]   |\n");
-	print_str(" +--------------------------------------------+\n");
-	print_str(" |        2024-2025 Nyxieworld Project        |\n");
-	print_str(" +--------------------------------------------+\n");
-	print_str(" |-----------PRESS [ENTER] TO REBOOT----------|\n");
-	print_str(" +--------------------------------------------+\n");
-
+                  //Text Color        //Background Color
+  print_set_color(PRINT_COLOR_WHITE, PRINT_COLOR_BLACK);
+  print_str(" +-------------------------------------------------------------+\n");
+  print_str(" |                                                             |\n");
+	print_str(" |             [NyxieworldOS Version 0.0.2 <Allana>]           |\n");
+	print_str(" |                2024-2025 Nyxieworld Project                 |\n");
+	print_str(" |             (C) Copyright Nyxieworld Productions            |\n");
+	print_str(" |                                                             |\n");
+	print_str(" +-------------------------------------------------------------+\n");
+  print_str(" [                  Development Release 0.0.2                  ]\n");
 }
